4779f49173e81179a10e99129aa1de5431c5f1a4
green_tea <vetlin.vladislav@yandex.ru>
2020-02-07 13:39:55 +0300

    add inodes to filesystem

--- filesystem/configs.h
+++ filesystem/configs.h
@@ -1,6 +1,15 @@
 #pragma once
 
+// SECTORS
 const int SECTOR_SIZE = 32;
 const int SECTOR_COUNT = 64; // 2KB (1024/32)
-const int SYSTEM_SIZE = 2048;
+const int SECTORS_FULL_SIZE = 2048;
+
+// MAPPERS
 const int SECTOR_MAP_SIZE = 4;
+const int INODE_MAP_SIZE = 4;
+
+// INODES
+const int INODE_SIZE = 32;
+const int INODE_COUNT = 64;
+const int INODES_FULLSIZE = 2048;
--- filesystem/system.h
+++ filesystem/system.h
@@ -10,19 +10,31 @@ struct Filesystem {
 };
 
 unsigned char* init_filesystem(struct Filesystem* fs) {
-    fs->filesystem = (unsigned char*)malloc(SYSTEM_SIZE);
+    fs->sectors = (unsigned char*)malloc(SECTORS_FULL_SIZE);
     fs->sector_map = (unsigned char*)malloc(SECTOR_MAP_SIZE);
+    fs->inode_map = (unsigned char*)malloc(INODE_MAP_SIZE);
+    fs->inode_system = (unsigned char*)malloc(INODE_FULL_SIZE);
 }
 
 unsigned char* get_sector_map(struct Filesystem* fs) {
     return fs->sector_map;
 }
 
-unsigned char* get_filesystem(struct Filesystem* fs) {
-    return fs->filesystem;
+unsigned char* get_sectors(struct Filesystem* fs) {
+    return fs->sectors;
+}
+
+unsigned char* get_inode_map(struct Filesystem* fs) {
+    return fs->inode_map;
+}
+
+unsigned char* get_inode_system(struct Filesystem* fs) {
+    return fs->inode_system;
 }
 
 void free_filesystem(struct Filesystem* fs) {
     free(fs->filesystem);
     free(fs->sector_map);
+    free(fs->inode_map);
+    free(fs->inode_system);
 }
